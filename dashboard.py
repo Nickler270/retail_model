9b9708edefa9ddd0af64ef23127e0cb41572e8a7
Nickler270 <faniejr@zoho.com>
2025-05-14 00:12:48 +0200

    sec commit

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Retail Resource Allocation",  
     page_icon="public/webgav icon_dark.png", 
-    layout="wide", 
+    layout="centered", 
     initial_sidebar_state="auto" 
 )
 
